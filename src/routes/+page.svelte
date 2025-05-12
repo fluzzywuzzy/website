bdd9a2dfa65c5c631bdaf3dd7cd1e8729e049baf
Vincent Nord Lundström <vnl@kth.se>
2025-05-12 14:23:57 +0200

    Add construction page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Under construction</h1>
-<p>Want to get in touch with me? Send me an email! <a href="mailto:vnl@kth.se">vnl@kth.se</a></p>
+<script>
+    import construction from '$lib/construction.gif';
+    import '/src/scss/construction.scss';
+</script>
+
+<svelte:head>
+	<title>Vincent Nord Lundström</title>
+</svelte:head>
+
+<div class="construction-wrapper">
+    <h1>Under construction</h1>
+    <img alt="Animation of man using powertools" src={construction}/>
+    <p>Want to get in touch with me? Send me an email! <a href="mailto:vnl@kth.se">vnl@kth.se</a></p>
+</div>
